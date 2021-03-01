59c18b5f7e8f4d19933f9e9a35d7ce82bfeab439
farhan-nahid <nahidahmed9933@gmail.com>
2021-03-02 03:10:00 +0600

    half

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,53 @@
+// phone decrement
+
 document
   .getElementById("phone-decrement")
   .addEventListener("click", function () {
-    console.log("click me");
+    const phoneInput = document.getElementById("phone-count");
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount - 1;
+    phoneInput.value = phoneNewCount;
+    const phonePrize = phoneNewCount * 1219;
+    document.getElementById("phone-prize").innerText = "$" + phonePrize;
   });
+
+// phone increment
+
+document
+  .getElementById("phone-increment")
+  .addEventListener("click", function () {
+    const phoneInput = document.getElementById("phone-count");
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount + 1;
+    phoneInput.value = phoneNewCount;
+    const phonePrize = phoneNewCount * 1219;
+    document.getElementById("phone-prize").innerText = "$" + phonePrize;
+  });
+
+// case decrement
+
+document
+  .getElementById("case-decrement")
+  .addEventListener("click", function () {
+    const caseInput = document.getElementById("case-count");
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount - 1;
+    caseInput.value = caseNewCount;
+    const casePrize = caseNewCount * 59;
+    document.getElementById("case-prize").innerText = "$" + casePrize;
+  });
+
+// case increment
+
+document
+  .getElementById("case-increment")
+  .addEventListener("click", function () {
+    const caseInput = document.getElementById("case-count");
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+    casePrize = caseNewCount * 59;
+    document.getElementById("case-prize").innerText = "$" + casePrize;
+  });
+
+function calculateTotal() {}

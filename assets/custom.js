e6f6accee99d3a057bcb83e00dab271f7eb1c510
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-03 13:07:58 +0600

    case added

--- assets/custom.js
+++ assets/custom.js
@@ -1,81 +1,40 @@
-// phone decrement
+// phone
 
-document
-  .getElementById("phone-decrement")
-  .addEventListener("click", function () {
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value);
-    if (phoneCount > 0) {
-      const phoneNewCount = phoneCount - 1;
-      phoneInput.value = phoneNewCount;
-      const phonePrize = phoneNewCount * 1219;
-      document.getElementById("phone-prize").innerText = "$" + phonePrize;
-      const subTotal = (document.getElementById("total__subtotal").innerText =
-        phonePrize);
-      const subTotalNumber = parseInt(subTotal);
-      const tax = document.getElementById("tax").innerText;
-      const taxNumber = parseInt(tax) * 0.1;
-      console.log(subTotalNumber, taxNumber);
-      document.getElementById("total__cost").innerText = phonePrize + taxNumber;
-    } else {
-      alert("You can't decrement lase then 0 !!");
-    }
-  });
+document.getElementById("phone__decrement").addEventListener("click", () => {
+  const phoneCount = document.getElementById("phone__count");
+  const phoneCountNumber = parseInt(phoneCount.value);
+  phoneCount.value = phoneCountNumber - 1;
+});
 
-// phone increment
+document.getElementById("phone__increment").addEventListener("click", () => {
+  const phoneCount = document.getElementById("phone__count");
+  const phoneCountNumber = parseInt(phoneCount.value);
+  phoneCount.value = phoneCountNumber + 1;
+});
 
-document
-  .getElementById("phone-increment")
-  .addEventListener("click", function () {
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount + 1;
-    phoneInput.value = phoneNewCount;
-    const phonePrize = phoneNewCount * 1219;
-    document.getElementById("phone-prize").innerText = "$" + phonePrize;
-    const subTotal = (document.getElementById("total__subtotal").innerText =
-      phonePrize);
-    const subTotalNumber = parseInt(subTotal);
-    const tax = document.getElementById("tax").innerText;
-    const taxNumber = parseInt(tax) * 0.1;
-    console.log(subTotalNumber, taxNumber);
-    document.getElementById("total__cost").innerText = phonePrize + taxNumber;
-  });
+// case
 
-// case decrement
+document.getElementById("case__decrement").addEventListener("click", () => {
+  const caseCount = document.getElementById("case__count");
+  const caseCountNumber = parseInt(caseCount.value);
+  if (caseCountNumber == 0) {
+    const error = document.getElementById("case__error");
+    error.style.display = "block";
+    error.innerText = "Please Increment. You can not buy less then 0";
+    error.style.padding = "10px";
+    error.style.textAlign = "center";
+    error.style.backgroundColor = "rgba(255, 0, 0, 0.238)";
+    error.style.color = "red";
+    error.style.marginTop = "10px";
+  } else if (caseCountNumber > 0) {
+    caseCount.value = caseCountNumber - 1;
+  }
+});
 
-document
-  .getElementById("case-decrement")
-  .addEventListener("click", function () {
-    const caseInput = document.getElementById("case-count");
-    const caseCount = parseInt(caseInput.value);
-    if (caseCount > 0) {
-      const caseNewCount = caseCount - 1;
-      caseInput.value = caseNewCount;
-      const casePrize = caseNewCount * 59;
-      document.getElementById("case-prize").innerText = "$" + casePrize;
-      document.getElementById("total__subtotal").innerText = casePrize;
-      const tax = document.getElementById("tax").innerText;
-      const taxNumber = parseInt(tax) * 0.1;
-      document.getElementById("total__cost").innerText = casePrize + taxNumber;
-    } else {
-      alert("You can't decrement lase then 0 !!");
-    }
-  });
-
-// case increment
-
-document
-  .getElementById("case-increment")
-  .addEventListener("click", function () {
-    const caseInput = document.getElementById("case-count");
-    const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount + 1;
-    caseInput.value = caseNewCount;
-    casePrize = caseNewCount * 59;
-    document.getElementById("case-prize").innerText = "$" + casePrize;
-    document.getElementById("total__subtotal").innerText = casePrize;
-    const tax = document.getElementById("tax").innerText;
-    const taxNumber = parseInt(tax) * 0.1;
-    document.getElementById("total__cost").innerText = casePrize + taxNumber;
-  });
+document.getElementById("case__increment").addEventListener("click", () => {
+  const error = document.getElementById("case__error");
+  error.style.display = "none";
+  const caseCount = document.getElementById("case__count");
+  const caseCountNumber = parseInt(caseCount.value);
+  caseCount.value = caseCountNumber + 1;
+});

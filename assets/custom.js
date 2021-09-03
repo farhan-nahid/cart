e10da15b9ee764e0f89660018965ca9eaf494d33
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-03 11:26:52 +0600

    not compleate

--- assets/custom.js
+++ assets/custom.js
@@ -5,10 +5,21 @@ document
   .addEventListener("click", function () {
     const phoneInput = document.getElementById("phone-count");
     const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount - 1;
-    phoneInput.value = phoneNewCount;
-    const phonePrize = phoneNewCount * 1219;
-    document.getElementById("phone-prize").innerText = "$" + phonePrize;
+    if (phoneCount > 0) {
+      const phoneNewCount = phoneCount - 1;
+      phoneInput.value = phoneNewCount;
+      const phonePrize = phoneNewCount * 1219;
+      document.getElementById("phone-prize").innerText = "$" + phonePrize;
+      const subTotal = (document.getElementById("total__subtotal").innerText =
+        phonePrize);
+      const subTotalNumber = parseInt(subTotal);
+      const tax = document.getElementById("tax").innerText;
+      const taxNumber = parseInt(tax) * 0.1;
+      console.log(subTotalNumber, taxNumber);
+      document.getElementById("total__cost").innerText = phonePrize + taxNumber;
+    } else {
+      alert("You can't decrement lase then 0 !!");
+    }
   });
 
 // phone increment
@@ -22,6 +33,13 @@ document
     phoneInput.value = phoneNewCount;
     const phonePrize = phoneNewCount * 1219;
     document.getElementById("phone-prize").innerText = "$" + phonePrize;
+    const subTotal = (document.getElementById("total__subtotal").innerText =
+      phonePrize);
+    const subTotalNumber = parseInt(subTotal);
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax) * 0.1;
+    console.log(subTotalNumber, taxNumber);
+    document.getElementById("total__cost").innerText = phonePrize + taxNumber;
   });
 
 // case decrement
@@ -31,10 +49,18 @@ document
   .addEventListener("click", function () {
     const caseInput = document.getElementById("case-count");
     const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount - 1;
-    caseInput.value = caseNewCount;
-    const casePrize = caseNewCount * 59;
-    document.getElementById("case-prize").innerText = "$" + casePrize;
+    if (caseCount > 0) {
+      const caseNewCount = caseCount - 1;
+      caseInput.value = caseNewCount;
+      const casePrize = caseNewCount * 59;
+      document.getElementById("case-prize").innerText = "$" + casePrize;
+      document.getElementById("total__subtotal").innerText = casePrize;
+      const tax = document.getElementById("tax").innerText;
+      const taxNumber = parseInt(tax) * 0.1;
+      document.getElementById("total__cost").innerText = casePrize + taxNumber;
+    } else {
+      alert("You can't decrement lase then 0 !!");
+    }
   });
 
 // case increment
@@ -48,6 +74,8 @@ document
     caseInput.value = caseNewCount;
     casePrize = caseNewCount * 59;
     document.getElementById("case-prize").innerText = "$" + casePrize;
+    document.getElementById("total__subtotal").innerText = casePrize;
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax) * 0.1;
+    document.getElementById("total__cost").innerText = casePrize + taxNumber;
   });
-
-function calculateTotal() {}
